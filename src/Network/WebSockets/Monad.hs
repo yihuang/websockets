7e0f0fe8f3c891f6bf348657172a63722186bf29
yihuang <yi.codeplayer@gmail.com>
2011-11-15 12:57:13 +0800

    expose WebSocketsEnv constructor internally.

--- src/Network/WebSockets/Monad.hs
+++ src/Network/WebSockets/Monad.hs
@@ -4,6 +4,7 @@
 module Network.WebSockets.Monad
     ( WebSocketsOptions (..)
     , defaultWebSocketsOptions
+    , WebSocketsEnv (..)
     , WebSockets (..)
     , runWebSockets
     , runWebSocketsWith
