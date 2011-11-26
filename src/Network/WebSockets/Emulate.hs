8578ff8e0fa861fd7be228b982ad6e79f6068eeb
yihuang <yi.codeplayer@gmail.com>
2011-11-26 21:04:50 +0800

    Chan -> TChan

--- src/Network/WebSockets/Emulate.hs
+++ src/Network/WebSockets/Emulate.hs
@@ -13,29 +13,29 @@ import Prelude hiding (catch)
 import Data.ByteString (ByteString)
 import Data.Enumerator
 import Control.Exception
-import Control.Concurrent.Chan
+import Control.Concurrent.STM
 import Network.WebSockets.Handshake.Http (Request, RequestHttpPart)
 import Network.WebSockets.Monad
 import Network.WebSockets.Protocol
 import Network.WebSockets.Protocol.Emulate (EmulateProtocol(..))
 import Control.Monad.Trans (liftIO)
 
-type StreamChan a = Chan (Stream a)
+type StreamChan a = TChan (Stream a)
 
 iterChan :: StreamChan a -> Iteratee a IO ()
 iterChan ch = continue go
   where
-    go EOF = liftIO $ writeChan ch EOF
-    go stream = liftIO (writeChan ch stream) >> continue go
+    go EOF = liftIO $ atomically $ writeTChan ch EOF
+    go stream = liftIO (atomically $ writeTChan ch stream) >> continue go
 
 enumChan :: StreamChan a -> Enumerator a IO b
 enumChan ch = checkContinue0 $ \loop f -> do
-    stream <- liftIO $ readChan ch
+    stream <- liftIO $ atomically $ readTChan ch
     f stream >>== loop
 
 runEmulator :: StreamChan ByteString -> StreamChan ByteString -> RequestHttpPart -> (Request -> WebSockets EmulateProtocol a) -> IO a
 runEmulator inChan outChan req app = do
     r <- (run $ enumChan inChan $$ runWebSockets req app $ iterChan outChan)
            `catch` (ioError)
-           `finally` (writeChan outChan EOF)
+           `finally` (atomically $ writeTChan outChan EOF)
     either (error . show) return r
