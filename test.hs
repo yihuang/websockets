1fc38410e0e21b219e9ad5766c281612309198d3
yihuang <yi.codeplayer@gmail.com>
2012-02-19 02:05:45 +0800

    run as tcp server along with websockets server.

--- test.hs
+++ test.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 import Data.ByteString (ByteString)
-import Data.Conduit ( ($$) )
 import qualified Blaze.ByteString.Builder as B
+import Data.Conduit ( ($$) )
+import qualified Data.Conduit.Network as Net
+import Control.Concurrent (forkIO)
 
 import Network.WebSockets.Conduit as WS
 import Network.Wai.Handler.Warp
 import Network.HTTP.Types (status200)
 import Network.Wai (Response(ResponseBuilder))
 
-echo :: WS.Request -> WS.Application ByteString
-echo _ src snk = src $$ snk
+echo :: WS.Application ByteString
+echo src snk = src $$ snk
 
 main :: IO ()
-main = runSettings defaultSettings
+main = do
+    forkIO $ Net.runTCPServer 
+                (Net.ServerSettings 3001 Nothing)
+                echo
+    runSettings defaultSettings
          { settingsPort = 3000
-         , settingsIntercept = intercept (undefined::WS.Hybi00) (WebSocketsOptions (return ())) echo
+         , settingsIntercept = intercept (undefined::WS.Hybi00) (WebSocketsOptions (return ())) (const echo)
          } (\_ -> return $ ResponseBuilder status200 [] (B.fromByteString "Pong"))
